bd80ca74f3da353bb5a72f2ab1ba8aee933db798
Jikrou <jamaleddine.zait@gmail.com>
2024-06-05 19:15:48 +0000

    Update height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,16 +10,17 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+
 	if (tree->left != NULL)
 	{
-		h++;
 		binary_tree_height(tree->left);
+		h++;
 	}
 
 	if (tree->right != NULL)
 	{
-		h++;
 		binary_tree_height(tree->left);
+		h++;
 	}
 	else
 		return (h);
